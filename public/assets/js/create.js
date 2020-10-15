e6320ad7a48840d45ee25c808a4574958c5b2928
dfabert <glen.fabert@gmail.com>
2020-10-15 19:11:06 -0400

    added create.js and variables

---
+++ public/assets/js/create.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var creatorEmail = $('creatorEmail');
+    var creatorPasswork = $('creatorPassword');
+    var partyName = $('partyName');
+    var partyDescription = $('partyDescription');
+    var userPassword = $('userPassword');
+
+});
