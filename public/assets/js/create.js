9779bdb6dfccd43b4aaf822b308247089d352778
dfabert <glen.fabert@gmail.com>
2020-10-15 20:56:10 -0400

    correct two erros in create.js

--- public/assets/js/create.js
+++ public/assets/js/create.js
@@ -14,34 +14,34 @@ $(document).ready(function() {
             creatorPassword: creatorPassword.val().trim(),
             userPassword: userPassword.val().trim(),
         };
-        createParty(partyData)
-    };
+        createParty(partyData);
+    });
 
     function createParty(partyData) {
         $.post('api/parties', partyData)
             .then(getParties);
     };
 
-    function createPartyRow(partyData) {
+    function createPartyRow(partyName) {
         var newTr = $("<tr>");
-        newTr.data('party', partyData:name);
+        newTr.data('party', partyName);
         return newTr;
-    }
+    };
 
     function renderPartyList(rows) {
         partyList.prepend(rows);
-    }
+    };
 
     function getParties() {
         $.get("/api/parties", function(data) {
             var rowsToAdd = [];
             for (var i = 0; i < data.length; i++) {
-              rowsToAdd.push(createPartyRow(data[i]));
+              rowsToAdd.push(createPartyRow(data[i].name));
             }
             renderPartyList(rowsToAdd);
             nameInput.val("");
           });
-    }
+    };
 
 
 
